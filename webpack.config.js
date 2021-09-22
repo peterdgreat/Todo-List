b28ec6bc1db93ff6e7837250227bceb9e9a801b7
Ajayi Peter <peterdgreat2016@gmail.com>
2021-09-22 04:37:49 +0200

    Javascript Array created
    - A seperate file craeted for a class of tasks
    - tasks imported in the index file

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-    mode: 'development',
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
-   devtool: 'inline-source-map',
-   devServer: {
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/tasks.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Development',
-      template: './src/index.html'
+      title: 'Development',
+      template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-     publicPath: '/',
-   },
-   module: {
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
@@ -31,4 +31,4 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
       },
     ],
   },
- };
+};
