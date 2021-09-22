4097cb411eb6991dfcc3b0c482fbebcefe4e2440
Ajayi Peter <peterdgreat2016@gmail.com>
2021-09-22 13:29:41 +0200

    Todo content and styling added
    - all  li content generated from js
    -bootstrap added for styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-import _ from 'lodash';
 import Tasks from './tasks.js';
 import './style.css';
 
-const tasksData = [new Tasks('Watch Movie', false, 0), new Tasks('Read a book', true, 1), new Tasks('Practice Algorithms', false, 2)];
+let id = 0;
+const tasksData = [new Tasks('Watch Movie', false, id += 1), new Tasks('Read a book', true, id += 1), new Tasks('Practice Algorithms', false, id += 1)];
+
+const showTasks = (() => {
+  const tasksList = document.querySelector('.tasks');
+  tasksData.forEach((task) => {
+    const li = document.createElement('li');
+    const input = document.createElement('input');
+    input.type = 'checkbox';
+    input.checked = task.completed;
+    input.addEventListener('change', () => {
+      task.completed = input.checked;
+      if (task.completed === true) {
+        li.classList.add('completed');
+      }
+    });
+    li.appendChild(input);
+
+    const description = document.createElement('p');
+    description.innerText = task.description;
+    li.appendChild(description);
+
+    const more = document.createElement('span');
+    more.classList.add('material-icons');
+    more.innerText = 'more_vert';
+    li.appendChild(more);
+
+    li.classList.add('d-flex', 'justify-content-between', 'border-m');
+    tasksList.appendChild(li);
+    console.log(task.index);
+  });
+  const li = document.createElement('li');
+  const p = document.createElement('p');
+  p.innerText = 'Clear all completed';
+  li.appendChild(p);
+  li.classList.add('d-flex', 'justify-content-around', 'bg');
+  tasksList.appendChild(li);
+});
+window.onload = () => {
+  showTasks();
+};
