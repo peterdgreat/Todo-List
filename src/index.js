5afc23c530ad34594012be14fdb52e37d2d93ba2
Ajayi Peter <peterdgreat2016@gmail.com>
2021-09-22 02:47:40 +0200

    Webpack setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import _ from 'lodash';
+import printMe from './print.js';
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+     // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+  element.appendChild(btn);
+  element.classList.add('body');
+    return element;
+  }
+  
+  document.body.appendChild(component());
