dd9ad7edc3079d9b1e260ac110e51cc5032ee052
Ajayi Peter <peterdgreat2016@gmail.com>
2021-09-22 03:15:06 +0200

    Linters Setup

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import printMe from './print.js';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-    const btn = document.createElement('button');
-     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
   element.appendChild(btn);
   element.classList.add('body');
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  return element;
+}
+
+document.body.appendChild(component());
