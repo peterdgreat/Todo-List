02a9a3af21ab40a640f0d70d5b8de83544246c9a
Ajayi Peter <peterdgreat2016@gmail.com>
2021-09-24 16:52:26 +0200

    Input text value added
    - input value from tasks.js
    -loacal storage class moved to tasks.js
    -id added using length of array

--- src/tasks.js
+++ src/tasks.js
@@ -1,19 +1,90 @@
 import './style.css';
 
+// let id = 0;
 export default class Tasks {
-  constructor(description, completed, index) {
-    this.description = description;
-    this.completed = completed;
-    this.index = index;
-  }
+    static tasks = [];
 
-  statusChanges(li) {
-    if (this.completed === true) {
-      li.classList.add('completed');
-      this.completed = true;
-    } else {
-      li.classList.remove('completed');
-      this.completed = false;
+    constructor(description, completed = false, index = Tasks.tasks.length) {
+      this.description = description;
+      this.completed = completed;
+      this.index = index;
     }
-  }
+
+    statusChanges(li) {
+      if (this.completed === true) {
+        li.classList.add('completed');
+        this.completed = true;
+      } else {
+        li.classList.remove('completed');
+        this.completed = false;
+      }
+    }
+
+    static setLocal() {
+      localStorage.setItem('tasks', JSON.stringify(Tasks.tasks));
+    }
+
+    static showTasks() {
+      const ul = document.querySelector('.tasks');
+      ul.innerHTML = '';
+      Tasks.tasks.forEach((task) => {
+        const li = document.createElement('li');
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.checked = task.completed;
+        if (task.completed === true) {
+          li.classList.add('completed');
+        } else {
+          li.classList.remove('completed');
+        }
+        input.addEventListener('change', () => {
+          task.completed = input.checked;
+          new Tasks().statusChanges(li);
+          if (task.completed === true) {
+            li.classList.add('completed');
+          }
+          Tasks.setLocal();
+        });
+        li.appendChild(input);
+
+        const description = document.createElement('p');
+        description.innerText = task.description;
+        li.appendChild(description);
+
+        const more = document.createElement('span');
+        more.classList.add('material-icons');
+        more.innerText = 'more_vert';
+        li.appendChild(more);
+
+        li.classList.add('d-flex', 'justify-content-between', 'border-m', 'align-items-center', 'text-center');
+
+        ul.appendChild(li);
+      });
+      const li = document.createElement('li');
+      const p = document.createElement('p');
+      p.innerText = 'Clear all completed';
+      li.appendChild(p);
+      li.classList.add('d-flex', 'justify-content-around', 'bg');
+      ul.appendChild(li);
+    }
+
+    static getLocal() {
+      // get local storage
+      const tasks = JSON.parse(localStorage.getItem('tasks'));
+      if (tasks) {
+        Tasks.tasks = tasks;
+      }
+      Tasks.showTasks();
+    }
+
+    addTasks() {
+      Tasks.tasks.push(this);
+      // remove repeated tasks
+      Tasks.tasks = Tasks.tasks.filter((task, index, self) => index === self.findIndex((t) => (
+        t.description === task.description
+      )));
+      Tasks.setLocal();
+      // add Tasks to dom
+      Tasks.showTasks();
+    }
 }
