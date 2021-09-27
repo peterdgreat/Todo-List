53728ebb312bacc39930521b3e50604ea3082e60
Ajayi Peter <peterdgreat2016@gmail.com>
2021-09-27 22:40:29 +0200

    rechanged

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   mode: 'production',
